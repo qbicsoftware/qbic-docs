2c9595d3be260f1e7e0ee64d7065494d568afa3a
chahuistle <delagarza@gmail.com>
2019-03-28 22:07:24 -0600

    [skip travis] Not ready, but gotta eat something

--- generate-javadocs.py
+++ generate-javadocs.py
@@ -3,8 +3,8 @@
 # Script to generate documentation javadocs using javasphinx
 # This script assumes that javasphinx has been installed.
 
-# Output of this script is to populate the "docs" folder. For each repository
-# in repositories.txt, a folder will be created. The folder structure looks like:
+# Output of this script is to populate the "docs" folder. For each submodule
+# in submodules.txt, a folder under docs will be created. The folder structure looks like:
 # 
 # .
 # └── docs
@@ -27,8 +27,7 @@
 #        │           └── FooBarClass.rst
 #        └── packages.rst
 
-import argparse, shutil, os, distutils
-from distutils import dir_util
+import argparse, os, suprocess
 
 # useful constants
 # how many spaces should we write before writing each repo name
@@ -37,43 +36,94 @@ REPOSITORY_INDENTATION_LEVEL = 3
 REPOSITORIES_PLACEHOLDER = "$QBIC_REPOSITORIES$"
 # name of the master document
 MASTER_DOCUMENT_NAME = "index.rst"
+# directory where all git submodules reside
+SUBMODULE_FOLDER = "modules"
+# folder that has the source code, relative to each submodule folder
+SOURCE_DIR = 'src/java/main'
 
 # parses arguments
 def main():
     parser = argparse.ArgumentParser(description='QBiC Javadoc Generator.', prog='generate-javadocs.py', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-r', '--repositories-file', required=True, default='repositories.txt',
-        help='File containing a list of repositories. For each of these repositories, javadocs will be generated.')
+    parser.add_argument('-s', '--submodules-file', required=True, default='submodules.txt',
+        help='File containing a list of submodules. For each submodule, javadocs will be generated.')
     parser.add_argument('-t', '--template-file', required=True, default='.index.rst.template',
         help='Template file to generate index.rst.')
     parser.add_argument('-o', '--output-dir', required=True, default='docs',
         help='Folder on which the javadocs will be generated.')
+    parser.add_argument('-b', '--branch', required=True, default='master',
+        help='Branch from which javadocs will be generated for each submodule.')
     args = parser.parse_args()
 
-    # read contents of the repos file into a list
-    repos_list = parse_repos_file(args.repositories_file)
-    # generate javadocs for each repo
-    for repo_name in repos_list:
-        generate_javadocs(args.output_dir, repo_name)
+    # read contents of the submodules file into a list
+    submodules_list = parse_submodules_file(args.repositories_file)
+    # for each repo, make sure that it has been added as a submodule, then
+    # update each submodule and generate javadocs for it
+    for submodule_name in submodules_list:
+        update_submodule(submodule_name)
+        generate_javadocs(args.output_dir, submodule_name)
     # update index.rst    
-    update_master_file(args.output_dir, args.template_file, repos_list)
+    update_master_file(args.output_dir, args.template_file, submodules_list)
     
-def parse_repos_file(repositories_file):
-    repo_names = []
-    f = open(repositories_file, "r")
+# Parses the file found at the given path, returns
+# a list where each element is a line in the file.
+# Lines starting with '#' are ignored
+def parse_submodules_file(submodules_file):
+    submodule_names = []
+    f = open(submodules_file, "r")
     for line in f:
         line = line.strip()
         # ignore comments
         if (line.startswith('#')):
             continue
-        repo_names.append(line)
+        submodule_names.append(line)
     f.close()
+    return submodule_names
 
-def generate_javadocs(output_dir, repo_name):
+# issues git commands to add/update a git submodule
+# using the passed branch
+def update_submodule(submodule_name, branch):
+    # relative path to the submodule folder
+    submodule_dir = get_submodule_dir(submodule_name)
+    # force-add submodules
+    execute(
+        ["git", "submodule", "add", "--force", "-b", branch, '../{}'.format(submodule_name), submodule_dir],
+        'Could not add submodule {}.'.format(submodule_name))    
+    # update submodule
+    execute(
+        ["git", "submodule", "update", "--remote", submodule_dir], 
+        'Could not update submodule {}.'.format(submodule_name))
 
-def update_master_file(master_file, template_file, repos_list):
+# generates javadocs for the given submodule
+def generate_javadocs(output_dir, submodule_name):
+    # each submodule gets a directory under output_dir (e.g., docs/foo-lib, docs/bar-service)
+    # make sure that the folder exists
+    javadoc_output_dir = get_javadoc_output_dir(output_dir, submodule_name)
+    src_dir = '{}/{}'.format(get_submodule_dir(submodule_name), SOURCE_DIR)
+    if not (os.path.exists(javadoc_output_dir)):
+        os.makedirs(javadoc_output_dir)
+    execute(['javasphinx-apidoc', '-o', javadoc_output_dir, '-t', submodule_name])
+    javasphinx-apidoc -o sample-lib -t sample-lib ~/Projects/QBiC/cookiecutter-templates-cli/generated/sample-cli/src/main/
+
+def update_master_file(master_file, template_file, submodules_list):
     # be nice and list repos alphabetically in index.rst    
-    sorted_repos_list = repos_list.sorted()
-    
+    sorted_submodules_list = submodules_list.sorted()
+
+# given a submodule, returns the path of the folder, relative to the master repo
+def get_submodule_dir(submodule_name):
+    return '{}/{}'.format(SUBMODULE_FOLDER, submodule_name)
+
+# given a submodule, returns the path of the corresponding javadoc output folder
+def get_javadoc_output_dir(base_dir, submodule_name):
+    return '{}/{}'.format(base_dir, submodule_name)
+
+# executes an external command, raises an exception if the return code is not 0
+def execute(command, error_message):
+    completed_process = subprocess.run(command, capture_output=True)
+    if (completed_process.returncode != 0):
+        raise Exception('{}.\nExit code={}.\nStderr={}\nStdout{}'.format(
+            error_message, completed_process.returncode, completed_process.stderr, completed_process.stdout))
+
+
 
 if __name__ == "__main__":
     main()
--- push-changes.sh
+++ push-changes.sh
@@ -10,7 +10,11 @@
  
 # This script commits changes done to the docs folder. When changes are committed,
 # ReadTheDocs.org will start a build and will update the published documentation.
+
+# we need to add a remote with credentials
 git remote rm origin
 git remote add origin https://$GITHUB_USERNAME:$GITHUB_ACCESS_TOKEN@github.com
 git add $1
+# instruct Travis to ignore this commit to avoid an infinite loop
 git commit -m "[skip travis] Automatic javadoc generation" -m "Travis build ID: $TRAVIS_BUILD_ID" -m "Travis build number: $TRAVIS_BUILD_NUMBER"
+git push -u origin master
